64c6b18c9f2386deadb556cb667ad29c13e20802
Dedric <dedricguest@gmail.com>
2013-03-15 15:41:23 -0400

    first mvc commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+    define('SERVER_ROOT' , '/var/www/mvc');
+
+
+    define('SITE_ROOT' , 'http://mvc.dev');
+
+    /**
+     * Fetch the router
+     */
+    require_once(SERVER_ROOT . '/controllers/' . 'router.php');
